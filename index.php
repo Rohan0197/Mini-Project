3bcd5cb70815c82e4ba25ce1d29ff70c2bbbc7d7
Rohan197 <rohandongre197@gmail.com>
2024-02-28 19:28:13 +0530

    adding test files

--- index.php
+++ index.php
@@ -1,26 +1,21 @@
+<?php
+    include("header.html");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="post" >
-        <label> quantity: </label><br>
-        <input type="text" name="quantity" >
-        <input type="submit" value="total">
-    </form>
+    This is the Home page<br>
+    Stuff about your home page can go here<br>
 </body>
 </html>
 <?php
-    $item = "pizza";
-    $price = 5.99;
-    $quantity= $_POST["quantity"];
-    $total= null;
-
-    $total = $quantity * $price;
-
-    echo "you have ordered {$quantity} x {$item}s<br>";
-    echo "your total is {$total}";
+    include("footer.html");
 ?>
+
+
