e597ee8bcd3e8b88e9b27a3a29b63bbf08cb095b
Rohan197 <rohandongre197@gmail.com>
2024-03-01 18:26:08 +0530

    #

--- website/database.php
+++ website/database.php
@@ -7,10 +7,7 @@
     $conn = "";
 
     try{
-        $conn = mysqli_connect($db_server, 
-                                                    $db_user, 
-                                                    $db_pass, 
-                                                    $db_name);
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
     }
     catch(mysqli_sql_exception){
         echo"Could not connect! <br>";
